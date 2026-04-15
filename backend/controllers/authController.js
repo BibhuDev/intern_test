56f5282b627377e8c0e4f3ac644d339a0c7044b3
BibhuDev <bibhuduttapanda7@gmail.com>
2026-04-15 11:31:37 +0530

    register working

---
+++ backend/controllers/authController.js
@@ -0,0 +1,35 @@
+import User from "../models/User.js";
+import argon2 from "argon2";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await argon2.hash(password);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        email: user.email
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
